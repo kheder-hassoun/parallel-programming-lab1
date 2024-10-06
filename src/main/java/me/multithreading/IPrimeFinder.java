1e949e62b8c2a75b6fe5a950b91653ae30ac1cc1
kheder <khdrhswn32@gmail.com>
2024-10-06 16:35:13 +0300

    Refactor Code , Move Prime finder classes to me. Multithreading package

--- src/main/java/me/multithreading/IPrimeFinder.java
+++ src/main/java/me/multithreading/IPrimeFinder.java
@@ -1,3 +1,5 @@
+package me.multithreading;
+
 import java.util.List;
 
 interface PrimeFinder {
--- src/main/java/me/multithreading/PrimFinderTask.java
+++ src/main/java/me/multithreading/PrimFinderTask.java
@@ -1,3 +1,5 @@
+package me.multithreading;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
