889e1aed6dc30ddac1b1162b9aea4d3a8bb284b4
kheder <khdrhswn32@gmail.com>
2024-10-07 17:00:57 +0300

    Refactor code , separate classes ImageProcessor PixelUtility

---
+++ src/main/java/me/latencyOptimization/ImageProcessor.java
@@ -0,0 +1,49 @@
+package me.latencyOptimization;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageProcessor {
+
+    public static void recolorMultithreaded(BufferedImage originalImage, BufferedImage resultImage, int numberOfThreads) {
+        List<Thread> threads = new ArrayList<>();
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight() / numberOfThreads;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            final int threadMultiplier = i;
+
+            Thread thread = new Thread(() -> {
+                int xOrigin = 0;
+                int yOrigin = height * threadMultiplier;
+                recolorImage(originalImage, resultImage, xOrigin, yOrigin, width, height);
+            });
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void recolorSingleThreaded(BufferedImage originalImage, BufferedImage resultImage) {
+        recolorImage(originalImage, resultImage, 0, 0, originalImage.getWidth(), originalImage.getHeight());
+    }
+
+    public static void recolorImage(BufferedImage originalImage, BufferedImage resultImage, int leftCorner, int topCorner, int width, int height) {
+        for (int x = leftCorner; x < leftCorner + width && x < originalImage.getWidth(); x++) {
+            for (int y = topCorner; y < topCorner + height && y < originalImage.getHeight(); y++) {
+                PixelUtils.recolorPixel(originalImage, resultImage, x, y);
+            }
+        }
+    }
+}
